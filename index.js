6bc2f34521b2981fbb59dfda8b047a4b73cc013e
feiyuan peng <feiyuan2987@163.com>
2026-04-27 14:55:55 +0800

    chore: add 3 pages

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-/**
- * @format
- */
 import './shim'
 import './src/app'
-// import './test'
-// import '@/utils/errorHandle'
-// import { Navigation } from 'react-native-navigation'
-// import App from './App'
+import { Navigation } from 'react-native-navigation'
+import { initFileStructure, syncLog } from 'srcNew/utils/fs'
+import HistoryScreen from 'srcNew/History'
+import InitScreen from 'srcNew/Init'
+import HomeScreen from 'srcNew/Home'
+import { PermissionsAndroid } from 'react-native'
 
-// Navigation.registerComponent('com.myApp.WelcomeScreen', () => App)
-// Navigation.events().registerAppLaunchedListener(() => {
-//   Navigation.setRoot({
-//     root: {
-//       stack: {
-//         children: [
-//           {
-//             component: {
-//               name: 'com.myApp.WelcomeScreen',
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   })
-// })
+/**
+ * 初始化逻辑：
+ * 1. 创建本地 json 文件，保存 AB 两面的名称（默认名称为 A、B）
+ */
+const init = () => {
+  PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+    {
+      title: 'read write storage',
+      message: 'App needs access to your storage ',
+      buttonNeutral: 'Ask Me Later',
+      buttonNegative: 'Cancel',
+      buttonPositive: 'OK'
+    }
+  )
+    .then(() => initFileStructure())
+    .then(() => syncLog())
+    .catch((err) => {
+      console.log(err.message)
+    })
+}
+
+Navigation.registerComponent('Home', () => HomeScreen)
+Navigation.registerComponent('Init', () => InitScreen)
+Navigation.registerComponent('History', () => HistoryScreen)
+
+Navigation.events().registerAppLaunchedListener(async (a) => {
+  init()
+
+  Navigation.setRoot({
+    root: {
+      bottomTabs: {
+        children: [
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'Home'
+                  }
+                }
+              ]
+            }
+          },
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'Init'
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  })
+})
